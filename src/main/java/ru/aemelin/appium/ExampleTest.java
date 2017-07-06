d7f8f3dc096d790d491ae7646850e61949282840
Alexey Emelin <dolf@yandex-team.ru>
2017-07-06 08:43:15 +0300

    initial commit

---
+++ src/main/java/ru/aemelin/appium/ExampleTest.java
@@ -0,0 +1,35 @@
+package ru.aemelin.appium;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.openqa.selenium.remote.CapabilityType.BROWSER_NAME;
+import static org.openqa.selenium.remote.CapabilityType.BROWSER_VERSION;
+
+/**
+ * Created by dolf on 05.07.17.
+ */
+public class ExampleTest {
+
+    @Test
+    public void simpleTest() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+        capabilities.setCapability(MobileCapabilityType.APP, "/opt/android-sdk/com-yandex-browser-lite-17-6-1-11-apkplz.com.apk");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        capabilities.setCapability(BROWSER_NAME, "wvga800");
+        capabilities.setCapability(BROWSER_VERSION, "24");
+
+        AppiumDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://localhost:4724/wd/hub"), capabilities);
+        System.out.println(driver.getPageSource());
+        driver.quit();
+    }
+}
